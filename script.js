c5e56d1f17d18e922c3d9aa85d5fa0eabfeaa961
Jaylynne <41392871+jaylynnemay@users.noreply.github.com>
2018-07-19 09:44:26 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function popup(){
+  alert("uwu hewwo! whats this? owo");
+
+}
+
+function picturechange(){
+
+  document.getElementById("theImage").src="https://i.kym-cdn.com/photos/images/original/001/384/541/1d8.jpg"
+
+}
